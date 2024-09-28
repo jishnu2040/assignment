5246cfd927146e4378c37eef228826b5e1594ddb
Jishnuraj R S <139771339+jishnu2040@users.noreply.github.com>
2024-09-28 20:36:16 +0530

    Update property()class.py

--- Oops/property()class.py
+++ Oops/property()class.py
@@ -20,3 +20,28 @@ c = Circle(5)
 print(c.radius)  # Calls get_radius() and prints 5
 c.radius = 10    # Calls set_radius(10)
 print(c.radius)  # Prints 10
+
+
+
+# propert() using validation
+
+class Employee:
+    def __init__(self, salary):
+        self._salary = salary
+
+    def get_salary(self):
+        return self._salary
+
+    def set_salary(self, value):
+        if value < 0:
+            raise ValueError("Salary cannot be negative")
+        self._salary = value
+
+    salary = property(get_salary, set_salary)
+
+e = Employee(5000)
+e.salary = 6000   # Valid
+# e.salary = -1000  # Raises ValueError: Salary cannot be negative
+
+
+
