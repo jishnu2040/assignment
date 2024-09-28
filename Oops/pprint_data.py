6d0a6f12fe8a39acf69aa80bfea8bda162a5e3fe
Jishnuraj R S <139771339+jishnu2040@users.noreply.github.com>
2024-09-28 14:47:51 +0530

    Create pprint_data.py

---
+++ Oops/pprint_data.py
@@ -0,0 +1,7 @@
+from pprint import pprint
+
+data = {'name': 'Alice', 'age': 30, 'hobbies': ['reading', 'hiking'], 
+        'education': {'degree': 'PhD', 'field': 'Computer Science', 'year': 2015}}
+
+print(data)
+pprint(data)
