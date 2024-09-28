daecc72111157d9fae8f7f8d4775d716e1f89319
Jishnuraj R S <139771339+jishnu2040@users.noreply.github.com>
2024-09-28 14:49:59 +0530

    Update pprint_data.py

--- Oops/pprint_data.py
+++ Oops/pprint_data.py
@@ -5,3 +5,18 @@ data = {'name': 'Alice', 'age': 30, 'hobbies': ['reading', 'hiking'],
 
 print(data)
 pprint(data)
+
+
+
+output:
+
+# print
+{'name': 'Alice', 'age': 30, 'hobbies': ['reading', 'hiking'], 'education': {'degree': 'PhD', 'field': 'Computer Science', 'year': 2015}}
+
+
+# pprint
+{'age': 30,
+ 'education': {'degree': 'PhD', 'field': 'Computer Science', 'year': 2015},
+ 'hobbies': ['reading', 'hiking'],
+ 'name': 'Alice'}
+
